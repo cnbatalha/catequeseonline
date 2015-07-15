e681cd3d6f3d52e59cb1203b7326e3f4a077c988
Carlos Nascimento <cnbatalha@gmail.com>
2015-07-15 09:23:16 -0400

    - Atualizando Repositorio

--- back-end/catequese.online/src/main/java/catequese/online/Application.java
+++ back-end/catequese.online/src/main/java/catequese/online/Application.java
@@ -1,15 +1,8 @@
 package catequese.online;
 
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-import catequese.online.controller.DefaultController;
-import catequese.online.repository.CatequistaRepository;
 
 //@Configuration
 
