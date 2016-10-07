a9a9f521a23977e47b25fa3ba61118c206d00297
Carlos Nascimento <cnbatalha@gmail.com>
2016-10-07 11:35:00 -0400

    cadastrando catequista

--- front-end/catequese.online/app/scripts/controllers/catequistaModule.js
+++ front-end/catequese.online/app/scripts/controllers/catequistaModule.js
@@ -19,11 +19,31 @@ catequizandoModule.controller('catequistaListController', function($scope, $http
 
 	// carrega todos os registros usando paginacao
 	var fetchRegistros = function(pageIndex) {
+		
+		$scope.registros = [];
 
-		webService.getCatequistaList(pageIndex).then(function(value) {
-			$scope.page.loadPage(value);
-			$scope.registros = $scope.page.content;
+		// conexao firebase catequizandos
+		var catequizandosRef = firebase.database().ref('catequistas').orderByChild("nome")
+		.startAt($scope.inputSearch)
+		.endAt($scope.inputSearch + "\uf8ff")
+		.limitToFirst(10);
+
+		catequizandosRef.once('value', function(data) {		
+
+			data.forEach(function(data) {
+		        // key will be "fred" the first time and "barney" the second time
+		        var key = data.key;
+		        // childData will be the actual contents of the child
+		        var childData = data.val();
+		        childData.key = key;
+
+		        $scope.registros.push(childData);
+		        $scope.$apply();
+
+		    }); 
+
 		});
+
 	};
 
 	fetchRegistros(0);
@@ -31,6 +51,101 @@ catequizandoModule.controller('catequistaListController', function($scope, $http
 })
 .controller('catequistaController', function($scope, $http, $routeParams, webService) {
 
+	var controller = this;
+	var idRegistro = $routeParams.id;
+
+	$scope.registro = {};
+
+	$scope.operacaoOK = false;
+	$scope.operacaoErro = false;
+
+	$scope.positions = [{lat:-3.121552,lng:-60.035365,title:""}];
+
+	// adiciona catequista
+	$scope.addRegistro = function() {
+
+
+		// cria novo registro de turmas
+		if ( idRegistro == undefined)
+		{
+			idRegistro  = firebase.database().ref("catequistas").push().getKey();
+			$scope.registro.key = idRegistro ;
+		}
+
+		var updates = {};
+		updates['/catequistas/' + idRegistro ] = $scope.registro;
+
+  		// atualiza turmas
+  		firebase.database().ref().update(updates);		
+
+	};
+
+	var fetchRegistro = function(id) {
+
+		// conexao firebase turmas
+		var cateqRef = firebase.database().ref('catequistas/' + $routeParams.id).orderByValue();
+
+		cateqRef.on('value', function(data) {
+			$scope.registro = data.val();
+			$scope.$apply();
+
+		});
+
+	};
+
+	// inicialia dados da tela
+	var resetForm = function() {
+		$scope.registro = {};
+	};
+
+	$scope.registros = [];
+
+	var fetchRegistrosTurma = function() {
+		webService.getTurmaCatequizandoList(idTurmaList).then(function(value) {
+			$scope.registros = value;
+			$scope.total = $scope.registros.length;
+		});
+	};
+
+	$scope.getGeoPosition = function()
+	{
+		
+		return $http.get('http://maps.googleapis.com/maps/api/geocode/json?address='+ $scope.catequizando.endereco + ' MANAUS',
+		{
+			headers : {authorization : undefined}
+		})
+		.then(function(value) {
+			console.log(value.data);
+
+			var retorno = value.data.results[0];
+
+			console.log(retorno);
+
+			var position = {};
+
+			position.lat = retorno.geometry.location.lat;
+			position.lng = retorno.geometry.location.lng;
+			position.title = $scope.catequizando.nome;
+
+			$scope.positions = [];
+			$scope.positions.push(position)	;
+
+			console.log($scope.positions);
+			return value.data;
+		});	
+	}
+
+	$scope.substrNome = function( nome )
+	{
+		return nome.substr(0,10) + '...';
+	};
+
+	fetchRegistro(idRegistro);
+	
+	//if ( $scope.listarTurma )
+	//{
+	//	fetchRegistrosTurma();
+	//}
 	
 
 });
